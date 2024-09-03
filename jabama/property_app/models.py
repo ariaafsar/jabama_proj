b0464d2baeb9ecfac1db9e9b03dd3977e936b720
aria <aria>
2024-09-03 18:12:59 +0330

    edited model

--- jabama/property_app/models.py
+++ jabama/property_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class property(models.Model):
+class Property(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
